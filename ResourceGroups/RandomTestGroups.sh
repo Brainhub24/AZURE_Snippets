1c200f109cadbee5f3eb713481c5b104df45b7d6
Jason Prime <600613.m6@gmail.com>
2023-03-31 16:12:39 +0200

    Update RandomTestGroups.sh

--- ResourceGroups/RandomTestGroups.sh
+++ ResourceGroups/RandomTestGroups.sh
@@ -2,14 +2,23 @@
 # Created by Jan Gebser (Brainhub24)
 # https://github.com/Brainhub24/AZURE_Snippets/ResourceGroups/RandomTestGroups.sh
 
-count=1
+# Prompt user for number of resource groups to create
+echo "TEST-SCENARIO | How many resource groups would you like to create for your test?"
+read num_groups
 
-while [ $count -le 100 ]
+# Generate a unique filename for the output log
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+log_filename="Created_ResourceGroups_${timestamp}.log"
+
+# Start creating the resource groups in the background
+count=1
+while [ $count -le $num_groups ]
 do
     randomName=$(( $RANDOM % 10000 ))
     echo "Creating resource group RG-$randomName"
-    az group create --name RG-$randomName --location eastus
+    az group create --name RG-$randomName --location eastus >> $log_filename 2>&1 &
     count=$(( $count + 1 ))
 done
 
-echo "All resource groups created."
+echo "Creating resource groups in the background..."
+echo "Check $log_filename for the list of created resource groups."
